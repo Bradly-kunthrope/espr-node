b81822f5b58f54d5fd01ef11d4e9635c23ba0fb1
Bradly-kunthrope <james.carballo@obf.ateneo.edu>
2018-01-19 13:19:24 +0800

    latest update, with headers but not yet working

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,14 @@ module.exports = (app) => {
     app.get('/api', (req, res) => res.status(200).send({
         message: 'Welcome to the users API!',
     }));
-    function createUser (last_name, first_name, email, password) 
-       { 
-           return app.post('/api/users', usersController.create)      
-        };
+
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    app.post('/api/users', usersController.create);
     
     app.get('/api/users', usersController.list);
     
--- src/form.js
+++ src/form.js
@@ -1,38 +1,65 @@
 import React from 'react';
 import { format } from 'util';
-import api from './routes';
+// import api from './routes';
+const axios = require('axios');
 
 export default class Form extends React.Component{
-    state = {
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: ""
+    constructor(props){
+        super(props);
+        this.state = {
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: ""
+        };
+        // this.handleFirstName = this.handleFirstName.bind(this);
+        // this.handleLastName = this.handleLastName.bind(this);
+        // this.handleEmail = this.handleEmail.bind(this);
+        // this.handlePassword = this.handlePassword.bind(this);
+        this.change = this.change.bind(this);
     }
+    
 
     change = e => {
+        // this.bind(e);
         this.props.onChange({ [e.target.name]: e.target.value });
         this.setState({
             [e.target.name]: e.target.value
-        });
+        }, console.log(this.state));
     };
+
+    
     
     onSubmit = e => {
         e.preventDefault();
         // this.props.onSubmit(this.state);
-        api.createUser(this.state.first_name, this.state.last_name, this.state.email, this.state.password);
-        this.setState({
-            first_name: "",
-            last_name: "",
-            email: "",
-            password: ""
-        });
-        this.props.onChange({
-            first_name: "",
-            last_name: "",
-            email: "",
-            password: ""
-        });
+        // api.createUser(this.state.first_name, this.state.last_name, this.state.email, this.state.password);
+        // this.setState({
+        //     first_name: e.target.value,
+        //     last_name: e.target.value,
+        //     email: e.target.value,
+        //     password: e.target.value
+        // });
+        // console.log(this.state);
+        axios.post('http://localhost:8000/api/users/', {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+            password: this.state.password   
+
+        })
+            .then(response => {
+                console.log(response, 'User added!');
+            })
+            .catch(err => {
+                console.log(err, 'User not added, try again');
+            });
+        // this.props.onChange({
+        //     first_name: "",
+        //     last_name: "",
+        //     email: "",
+        //     password: ""
+        // });
 
         
     };
